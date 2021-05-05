8b2af7cb9042d01b296af5e0fc2581388543c436
dikoleshubh <dikole.shubh@gmail.com>
2021-05-05 22:34:13 +0530

    Welcome to user registration

---
+++ program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////
+            Name ver = new Name();
+            MailVerification ver1 = new MailVerification();
+            Password ver2 = new Password();
+
+            Console.WriteLine("------Welcome to Regular expression----- ");
+
+
+
+
+
+
+
+        }
+    }
+}
