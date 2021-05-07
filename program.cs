1fc4077cff359cb9ed2e13d8915442b6a504e566
dikoleshubh <dikole.shubh@gmail.com>
2021-05-08 00:12:03 +0530

    User registration Option

--- program.cs
+++ program.cs
@@ -17,6 +17,21 @@ namespace ReX
 
             Console.WriteLine("------Welcome to Regular expression----- ");
 
+            Console.WriteLine("Enter your First name");
+            string firstname = Console.ReadLine();
+            ver.firstname(firstname);
+
+            Console.WriteLine("Enter your last name");
+            string Lastname = Console.ReadLine();
+            ver.Lastname(Lastname);
+
+            Console.WriteLine("Enter your EmailID");
+            string emailID = Console.ReadLine();
+            ver1.verificationemail(emailID);
+
+            Console.WriteLine("Enter your Password");
+            string passwordID = Console.ReadLine();
+            ver2.passwordvarification(passwordID);
 
 
 
