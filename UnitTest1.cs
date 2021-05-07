f2f91ffb9d4e879d95f741f9e392cb1a9e8eda13
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-05-08 03:17:24 +0530

    TEst

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,45 +3,71 @@ using System;
 
 namespace UnitTestProject4
 {
-    [TestClass]
-    public class UnitTest1
+      [TestClass]
+    public class Test
     {
-
-
-        /*  UC1:- As a User need to enter a valid First Name - First name starts with Cap and has minimum 3 characters
-         */
+        /// <summary>
+        /// Given Valid First Name should return true
+        /// </summary>
         [TestMethod]
-        [DataRow("Shubh", true)]   //add parameters using the [DataRow] attribute values from the [DataRow] attribute will be passed to the test method
-        [DataRow("Dikole", true)]
-        public void UnitTestPatternValidation_ValidName(string FirstName, bool expected)
+        public void GivenFirstNameShouldReturnHappy()
         {
-            string FirstNamePattern = @"^[A-Z][a-z ]{2,}$"; //Define Pattern
-            bool actual = Program.PatternValidation(FirstName, FirstNamePattern); //passing parameter
-
-            Assert.AreEqual(expected, actual);  // section verifies that the action of the method under test behaves as expected.
-        }
-        
-        
-        [TestMethod]
-        [DataRow("1252", "Invalid")]  //add parameters using the [DataRow] attribute values from the [DataRow] attribute will be passed to the test method
-        [DataRow("ok", "Invalid")]
-        [DataRow(".dfgjs", "Invalid")]
-        public void UnitTestPatternValidation_InvalidName(string FirstName, string expected)
-        {
-            try
-            {
-                string FirstNamePattern = @"^[A-Z][a-z ]{2,}$";  //Define Pattern
-                Program.PatternValidation(FirstName, FirstNamePattern);  
-            }
-            catch (InvalidDataException ex)
-            {
-
-                Assert.AreEqual(expected, ex.Message); // section verifies that the action of the method under test behaves as expected.
-            }
-
+            //Arrange
+          
+            bool expected = true;
+            //Act
+            bool output = UserDetail.validateFirstName("Ankita");
+            //Assert
+            Assert.AreEqual(expected, output);
         }
 
+        /// <summary>
+        /// Given Valid Last Name should return true
+        /// </summary>
+        [TestMethod]
+        public void GivenLastNameShouldReturnHappy()
+        {
+            bool expected = true;
+            bool output = UserDetail.validateLastName("Patil");
+            Assert.AreEqual(expected, output);
+        }
+
+        /// <summary>
+        /// Given Valid Email should return true
+        /// </summary>
+        [TestMethod]
+        public void GivenValidEmailIdShouldReturnHappy()
+        {
+            //Arrange
+            bool expected = true;
+            //Act
+            bool output = UserDetail.validateEmail("abc@gmail.com");
+           // Assert
+            Assert.AreEqual(expected, output);
+        }
 
 
+        /// <summary>
+        /// Given Valid Mobile No should return true
+        /// </summary>
+        [TestMethod]
+        public void GivenValidMobileShouldReturnHappy()
+        {
+            bool expected = true;
+            bool output =UserDetail.validateMobileNo("91 9146293697");
+            Assert.AreEqual(expected, output);
+        }
+
+        /// <summary>
+        /// Given Valid Password should return true
+        /// </summary>
+        [TestMethod]
+        public void GivenValidPasswordShouldReturnHappy()
+        {
+            bool expected = true;
+            bool output =UserDetail.validatePassword("Anki@123");
+            Assert.AreEqual(expected, output);
+        }
     }
+}
 }
