17e7f857cedc9085f5ec3d7d7cc96b23b5cb15d5
dikoleshubh <dikole.shubh@gmail.com>
2021-05-08 00:57:24 +0530

    Phone number Validation

---
+++ Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ReX
+{
+    class Number
+    {
+        static string num = "^(0/91)?[7-9][0-9]{9}$";
+        Regex numbers = new Regex(num);
+        public void number(string phonenumber)
+        {
+            if (numbers.IsMatch(phonenumber)) //// phone number Validation Check
+            {
+                Console.WriteLine("Number Is Valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name Is Invalid");
+            }
+        }
+    }
+}
